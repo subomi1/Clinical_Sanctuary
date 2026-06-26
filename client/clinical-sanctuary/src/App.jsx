19d481687f2ceec73c57740c7680d208f6875797
Obans Olasubomi <Second_last@Obanss-MacBook-Air.local>
2026-06-26 09:30:35 +0100

    sidebar started

--- client/clinical-sanctuary/src/App.jsx
+++ client/clinical-sanctuary/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Sidebar from './components/layouts/Sidebar'
 
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-5xl font-bold text-blue-600">
+    <div className="min-h-screen flex bg-gray-100">
+      <Sidebar />
+      <h1 className="text-5xl font-bold text-blue-100">
         Clinical Sanctuary
       </h1>
     </div>
