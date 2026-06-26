0e10f99f2431f4a745e4e3602a0caf0327c1350f
Obans Olasubomi <Second_last@Obanss-MacBook-Air.local>
2026-06-26 07:58:24 +0100

    first commit

---
+++ client/clinical-sanctuary/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-blue-600">
+        Clinical Sanctuary
+      </h1>
+    </div>
+  )
+}
+
+export default App
